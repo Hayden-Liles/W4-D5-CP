bddadd73eb10683bf3c54b7440db110fb1b9d6fa
HardlySalty <118101156+HardlySalty@users.noreply.github.com>
2023-02-11 15:31:19 -0700

    added the tempatures (C, F)

--- app/Controllers/WeatherController.js
+++ app/Controllers/WeatherController.js
@@ -1,7 +1,19 @@
+import { weatherService } from "../Services/WeatherService";
+import { Pop } from "../Utils/Pop";
 
+
+async function drawWeather(){
+    try {
+        await weatherService.drawWeather()
+        
+    } catch (error) {
+        console.error(error)
+        Pop.error(error)
+    }
+}
 
 export class WeatherController{
-    constructor(data) {
+    constructor() {
         
     }
 }
--- app/Services/WeatherService.js
+++ app/Services/WeatherService.js
@@ -1,6 +1,15 @@
+import { Pop } from "../Utils/Pop"
 
 
 class WeatherService{
+    drawWeather() {
+        try {
+            console.log('hi')
+        } catch (error) {
+            console.error(error)
+            Pop.error(error)
+        }
+    }
     
 }
 
