693a030a325e7b68187c0a9ceae10a4cb7d9826c
HardlySalty <118101156+HardlySalty@users.noreply.github.com>
2023-02-12 01:06:47 -0700

    confirmation on delete & count updates on check

--- app/Services/TasksServices.js
+++ app/Services/TasksServices.js
@@ -1,16 +1,30 @@
 import { appState } from "../AppState.js"
 import { Task } from "../Models/Task.js"
+import { Pop } from "../Utils/Pop.js"
 import { todoApi } from "./axiosService.js"
 
 
 class TasksServices{
-    
+    async deleteTask(taskId) {
+        try {
+            const res = await todoApi.delete(taskId)
+            let taskToDelete = appState.tasks.findIndex(e => e.id == taskId)
+            appState.tasks.splice(taskToDelete, 1)
+            appState.emit('tasks')
+        }
+        catch (error) {
+           console.error(error)
+           Pop.error(error)
+        }
+    }
+
     async updateTask(taskId) {
         const taskToUpdate = appState.tasks.find(e => e.id == taskId)
         // @ts-ignore
         const res = await todoApi.put(taskId, { completed: !taskToUpdate.completed })
         // @ts-ignore
         taskToUpdate.completed = !taskToUpdate?.completed
+        appState.emit('tasks')
     }
 
     async getTasks() {
